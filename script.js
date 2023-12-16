efcf28652c32ee641ac09a76dff1217be7121365
MoizzIqbal <122679949+MoizzIqbal@users.noreply.github.com>
2023-12-16 17:09:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function appendToResult(value) {
+    result.value+= value
+  }
+  
+  function clearResult() {
+    result.value = ""
+  }
+  function calculateResult() {
+    try {
+      result.value = eval(result.value);
+    } catch (error) {
+      result.value = "Error";
+    }
+  }
